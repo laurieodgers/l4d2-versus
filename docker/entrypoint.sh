91ecf6fa7a603c599a50b65a2615a2bec80732a9
Laurie Odgers <laurieodgers83@gmail.com>
2022-11-22 09:59:09 +1000

    updates

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+​
+if [[ -z "${RCON_PASSWORD}" ]]; then
+    echo "Error: No rcon password set"
+    exit 1
+fi
+​
+if [[ -z "${MAP_NAME}" ]]; then
+    echo "Error: No map name set"
+    exit 1
+fi
+​
 # Update Game
 ./steamcmd.sh +login anonymous +force_install_dir ./l4d2 +app_update 222860 +quit
-
+​
 # Server Config
-cat >> l4d2/left4dead2/cfg/server.cfg <<EOF
+cat > l4d2/left4dead2/cfg/server.cfg <<EOF
 hostname "${HOSTNAME}"
 sv_region ${REGION}
+rcon_password "${RCON_PASSWORD}"
 sv_logecho 1
+sm_cvar mp_gamemode versus
+sv_gametypes "versus"
+mp_gamemode "versus"
+
 EOF
-
-# Start Game
-cd l4d2 && ./srcds_run -console -game left4dead2 -port "$PORT" +maxplayers "$PLAYERS" +map "$MAP"
-
